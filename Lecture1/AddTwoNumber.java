850b8a9fea52d410a1ecd79b8e613108e3ed56f5
only-dsa <itsjannesar@gmail.com>
2025-07-31 17:38:48 +0530

    Lecture till User Input

---
+++ Lecture1/AddTwoNumber.java
@@ -0,0 +1,18 @@
+package Lecture1;
+import java.util.*;
+public class AddTwoNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		short f = sc.nextShort();
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int sum = a + b;
+//		System.out.println((int)sum);
+		System.out.println(f);
+		double avg = (double)sum / 2;
+		System.out.println(avg);
+	}
+
+}
---
+++ Lecture1/Practice.java
@@ -0,0 +1,31 @@
+package Lecture1;
+
+public class Practice {
+	public static void main(String[] yash) {
+//		System.out.println("Hello World"); // print karega then move to the next line
+//		System.out.print("Bye"); // print karega and remains on the same line
+//		System.out.println("Kaise Ho??");
+		
+//		byte sum; // declaration
+//		sum = 10; // initialization
+//		byte sum = 10;
+//		byte sum = 128;
+//		short s = 32768;
+//		long n = 2147483648l;
+//		float d = 127.0f;
+////		float f = 12.7;
+//		double e = 127;
+//		float f = 127; // 127
+//		char ch = 'a';
+//		int num = ch; // implicit 
+//		char ch1 = (char)num; // explicit
+		
+		float f = 128;
+//		byte b = (byte)-129;
+		System.out.println(ch1);
+//		int sum = (int)127.50;
+//		System.out.println(sum);
+//		System.out.println(Integer.MAX_VALUE);
+//		System.out.println(s);
+	}
+}

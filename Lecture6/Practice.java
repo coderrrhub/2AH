0741ac2af7ed3e9d2e3f3eec8da49ccfa9c9c820
only-dsa <itsjannesar@gmail.com>
2025-08-06 19:53:46 +0530

    Array

---
+++ Lecture6/Practice.java
@@ -0,0 +1,15 @@
+package Lecture6;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean[][] arr = new boolean[5][4];
+		int val = 10;
+		System.out.println(arr);
+		System.out.println(arr[0]);
+		System.out.println(arr[3][2]);
+//		System.out.println(arr[5]);  Array Index Out Of Boud
+	}
+
+}
